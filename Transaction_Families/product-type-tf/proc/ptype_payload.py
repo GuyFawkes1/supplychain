ddd85d6cfbe6721bf9076b7074e4673dcd08a0b3
Anukriti Goyal <Nikki@Anukritis-MacBook-Air.local>
2018-07-18 16:46:37 +0530

    completed payload file

--- Transaction_Families/product-type-tf/proc/ptype_payload.py
+++ Transaction_Families/product-type-tf/proc/ptype_payload.py
@@ -1,32 +1,59 @@
 from sawtooth_sdk.processor.exceptions import InvalidTransaction
 
-class WalPayload(object):
+class PtypePayload(object):
 
 	def __init__(self,payload):
 
 		try:
-			name,action,pubkey = payload.decode().split(",")
+			name, ptype, role, action, cu_add, nxt_add, time_stamp = payload.decode().split(",")
 		except ValueError:
 			raise InvalidTransaction("Invalid payload serialization")
 
-		#Add different type of exceptions
-
 		self._name = name
+		self._ptype = ptype
+		self._role = role
 		self._action = action
-		self._pubkey = pubkey
+		self._cu_add = cu_add
+		self._nxt_add = nxt_add
+		self._time_stamp = time_stamp
 
 	@staticmethod
 	def from_bytes(payload):
-		return WalPayload(payload=payload)
+		return PtypePayload(payload=payload)
 
+	# Returns the name of the item 
 	@property
 	def name(self):
 		return self._name
-	
+
+	# Returns the product type name of the item
+	@property
+	def ptype(self):
+		return self._ptype
+
+	# Returns the role of the user who made the transaction
+	@property
+	def role(self):
+		return self._role
+
+	# Returns the action that was led to the transaction being created
 	@property
 	def action(self):
 		return self._action
 
+	# Returns the human readable form of the user who initiated the transaction
+	# and the user who currently held the item 
 	@property
-	def pubkey(self):
-		return self._pubkey
+	def cu_add(self):
+		return self._cu_add
+
+	# Returns the human readable form of the user who will receive the item next
+	# and who the results of the transaction are sent to
+	@property
+	def nxt_add(self):
+		return self._nxt_add
+
+	# Returns the time stamp of the transaction
+	@property
+	def time_stamp(self):
+		return self._time_stamp
